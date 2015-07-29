e8d9215e5ebd9d056fd72afd04e420263f950e23
Eike Starkmann <starkmann@undkonsorten.com>
2015-07-29 17:19:49 +0200

    ADD: Page TSConfig

---
+++ Configuration/TsConfig/TemplateLayout.ts
@@ -0,0 +1,7 @@
+
+tx_addressmgmt.templateLayouts {
+  short = Referenzen Kurz-Liste (short)
+  # never rename the grid... - 1st 4 letters are used for layout-switch in General.html
+  gridRef = Referenzen Kacheln (gridRef)
+  gridNews = News Kacheln (gridNews)
+}
--- ext_localconf.php
+++ ext_localconf.php
@@ -17,4 +17,8 @@ if (!defined('TYPO3_MODE')) {
 	)
 );
 
+\TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addPageTSConfig(
+		'<INCLUDE_TYPOSCRIPT: source="FILE:EXT:' . $_EXTKEY . '/Configuration/TsConfig/TemplateLayout.ts">'
+);
+
 ?>
