bfc60c420534d4d9b9bd4f86e931064478ea880b
eike <eike@hal>
2016-07-27 15:30:30 +0200

    ADD: Version for typo3 v7.6

--- ext_emconf.php
+++ ext_emconf.php
@@ -20,19 +20,17 @@ $EM_CONF[$_EXTKEY] = array(
 	'shy' => '',
 	'priority' => '',
 	'module' => '',
-	'state' => 'alpha',
+	'state' => 'beta',
 	'internal' => '',
 	'uploadfolder' => '0',
 	'createDirs' => '',
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '1.0.0',
+	'version' => '2.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'extbase' => '6.0',
-			'fluid' => '6.0',
-			'typo3' => '6.0',
+			'typo3' => '7.6.0-7.99.99',
 		),
 		'conflicts' => array(
 		),
