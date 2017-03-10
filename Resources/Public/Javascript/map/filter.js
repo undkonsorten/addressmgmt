d3faa207f226e06c6fbc9ed09331550efa15ff14
karsten.nowak <nowak@undkonsorten.com>
2017-03-10 10:40:16 +0100

    ADD: update map only on form submit, reset button to form, fix error with empty map refs #12084

--- Resources/Public/Javascript/map/filter.js
+++ Resources/Public/Javascript/map/filter.js
@@ -6,9 +6,9 @@ $(document).ready(function() {
     $itemSelector = '.filter-list-item',
     $mapElement = $('[data-is-map=true]');
 
-  function checkSearchForm() {
+  function checkSearchForm(form) {
     filterArray = [];
-    $searchForm.find('select option:selected').each(function() {
+    form.find('select option:selected').each(function() {
       if($(this).attr('value') != 0) {
         filterArray.push(parseInt($(this).attr('value')));
       }
@@ -18,34 +18,40 @@ $(document).ready(function() {
       if(filterArray.length > 0) {
         for(i=0; i<filterArray.length;i++) {
           if (filterArray[i] != 0 && $.inArray(filterArray[i], item.data('filter')) < 0) {
-            item.hide().addClass('hide');
+            item.hide().addClass('hide').removeClass('visible');
             break;
           } else {
-            item.show().removeClass('hide');
+            item.show().removeClass('hide').addClass('visible');
           }
         }
       } else {
-       item.show().removeClass('hide');
+        item.show().removeClass('hide').addClass('visible');
       }
     });
-
-    if ($resultList.find($itemSelector + ':visible').length == 0) {
-      $('.map-filter .no-results').show();
+    if ($resultList.find($itemSelector + '.visible').length == 0) {
+      $('.no-results').show();
     } else {
-      $('.map-filter .no-results').hide();
+      $('.no-results').hide();
     }
   }
 
   if ($searchForm.length > 0) {
-    $searchForm.find('select.js-controlled').each(function () {
-      $(this).on('change', function (e) {
-        checkSearchForm();
-        $mapElement[0].dispatchEvent(new Event('update-list'));
-        $mapElement[0].dispatchEvent(new Event('fitbounds'));
+    $searchForm.each(function () {
+      form = $(this);
+      form.on('submit', function (e) {
+        console.log(e.type);
+        e.preventDefault();
+        checkSearchForm($(this));
+        if($mapElement.length) {
+          $mapElement[0].dispatchEvent(new Event('update-list'));
+          $mapElement[0].dispatchEvent(new Event('fitbounds'));
+        }
       });
-    });
-    $(window).on('load', function () {
-      checkSearchForm();
+      form.find('.reset').on('click', function (e) {
+        console.log(form);
+        form[0].reset();
+        form.submit();
+      });
     });
   }
 });
