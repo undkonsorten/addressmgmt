dd23de534557e50ffb8abb407ec4967ff67cbe4f
eike.starkmann <eike.starkmann@undkonsorten.com>
2018-10-23 14:14:28 +0200

    [TASK] create version 1.0.0 for 6.2

--- ext_emconf.php
+++ ext_emconf.php
@@ -27,12 +27,10 @@ $EM_CONF[$_EXTKEY] = array(
 	'modify_tables' => '',
 	'clearCacheOnLoad' => 0,
 	'lockType' => '',
-	'version' => '2.0.0',
+	'version' => '1.0.0',
 	'constraints' => array(
 		'depends' => array(
-			'extbase' => '6.0',
-			'fluid' => '6.0',
-			'typo3' => '6.0',
+			'typo3' => '6.2.0 - 6.99.99',
 		),
 		'conflicts' => array(
 		),
