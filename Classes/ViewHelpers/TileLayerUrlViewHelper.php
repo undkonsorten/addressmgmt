c0126945db75cbfa0ff4837e6528dfd4af00b365
eike.starkmann <eike.starkmann@undkonsorten.com>
2019-09-11 18:06:50 +0200

    [WIP] add semantic map

---
+++ Classes/ViewHelpers/TileLayerUrlViewHelper.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Undkonsorten\Addressmgmt\ViewHelpers;
+
+
+use TYPO3\CMS\Fluid\Core\Rendering\RenderingContextInterface;
+use TYPO3\CMS\Fluid\Core\ViewHelper\AbstractViewHelper;
+use TYPO3\CMS\Fluid\Core\ViewHelper\Facets\CompilableInterface;
+
+class TileLayerUrlViewHelper extends AbstractViewHelper implements CompilableInterface
+{
+    /**
+     * Constructor
+     *
+     * @api
+     */
+    public function __construct()
+    {
+        $this->registerArgument('additionalAttributes', 'array', 'Additional tag attributes. They will be added directly to the resulting HTML tag.', false);
+    }
+
+    /**
+     * Replaces newline characters by HTML line breaks.
+     *
+     * @return string the altered string.
+     * @api
+     */
+    public function render()
+    {
+        return static::renderStatic(
+            $this->arguments,
+            $this->buildRenderChildrenClosure(),
+            $this->renderingContext
+        );
+    }
+
+    /**
+     * @param array $arguments
+     * @param callable $renderChildrenClosure
+     * @param RenderingContextInterface $renderingContext
+     *
+     * @return string
+     */
+    public static function renderStatic(array $arguments, \Closure $renderChildrenClosure, RenderingContextInterface $renderingContext)
+    {
+        if (isset($arguments['additionalAttributes']) && is_array($arguments['additionalAttributes'])) {
+            $content = $arguments['additionalAttributes'];
+        } else {
+            $content = $renderChildrenClosure();
+        }
+        $url = str_replace('{id}',$content['options']['id'],$content['urlTemplate']);
+        $url = str_replace('{accessToken}',$content['options']['accessToken'],$url);
+
+        return $url;
+    }
+
+}
