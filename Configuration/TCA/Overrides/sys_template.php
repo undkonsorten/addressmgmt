fa13f80a926fe68248342eb120165097783144b8
karsten <nowak@undkonsorten.com>
2019-09-13 09:28:30 +0200

    [!!!][TASK] Exclude Basic css in extra typoscript file
    Basic styles are now excluded not used by default.
    They must be included in TypoScript if it should be used.
    
    Related: #14814

--- Configuration/TCA/Overrides/sys_template.php
+++ Configuration/TCA/Overrides/sys_template.php
@@ -4,5 +4,6 @@ if (!defined('TYPO3_MODE')) {
 }
 call_user_func(function ($packageKey) {
     \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($packageKey, 'Configuration/TypoScript', 'Address Management');
+    \TYPO3\CMS\Core\Utility\ExtensionManagementUtility::addStaticFile($packageKey, 'Configuration/TypoScript/BasicCss', 'Address Management Basic CSS');
 }, 'addressmgmt');
 
