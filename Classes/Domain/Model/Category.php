dab177e3fde770840002bfe0e978ce004994d149
Karsten Nowak <nowak@undkonsorten.com>
2024-08-21 17:55:03 +0200

    [BUGFIX] Fix error with filter categories and failed grouping
    Related: #26249

--- Classes/Domain/Model/Category.php
+++ Classes/Domain/Model/Category.php
@@ -2,7 +2,14 @@
 
 namespace Undkonsorten\Addressmgmt\Domain\Model;
 
+use TYPO3\CMS\Extbase\Annotation as Extbase;
+
 class Category extends \TYPO3\CMS\Extbase\Domain\Model\Category
 {
 
+    /**
+     * @var Category|null
+     * @Extbase\ORM\Lazy
+     */
+    protected $parent;
 }
