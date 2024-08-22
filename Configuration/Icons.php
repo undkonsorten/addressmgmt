66477d307e9a287a93ec14fcdc908aabb9c27c5b
Karsten Nowak <nowak@undkonsorten.com>
2024-08-22 16:20:27 +0200

    [TASK] Make plugin icon different by colors
    Related: #24586

--- Configuration/Icons.php
+++ Configuration/Icons.php
@@ -3,10 +3,10 @@ declare(strict_types=1);
 
 $iconList = [];
 foreach ([
-    'dash' => 'PluginList.svg',
-    'edit' => 'PluginList.svg',
-    'create' => 'PluginList.svg',
-    'show' => 'PluginList.svg',
+    'dash' => 'PluginDash.svg',
+    'edit' => 'PluginEdit.svg',
+    'create' => 'PluginCreate.svg',
+    'show' => 'PluginShow.svg',
     'list' => 'PluginList.svg',
  ] as $identifier => $path) {
     $iconList['ext-addressmgmt-plugin-'.$identifier.'-icon'] = [
