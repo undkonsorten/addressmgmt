a47d6d72ca855c0848e3a908e004ce691eb72269
barow <w@ba-ro.de>
2025-05-21 16:02:32 +0200

    [TASK] Lift core dependencies to 13.4.

--- ext_emconf.php
+++ ext_emconf.php
@@ -21,7 +21,7 @@ $EM_CONF[$_EXTKEY] = array(
     'version' => '6.1.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '11.5.0-12.99.99',
+            'typo3' => '10.4.0-13.99.99',
         ),
         'conflicts' => array(),
         'suggests' => array(
