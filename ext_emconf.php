fab2b4745a437199df28e97966ad165f9170e720
Karsten Nowak <nowak@undkonsorten.com>
2026-03-24 12:22:31 +0100

    [TASK] Bump version number to 6.1.1

--- ext_emconf.php
+++ ext_emconf.php
@@ -18,10 +18,10 @@ $EM_CONF[$_EXTKEY] = array(
     'author_email' => 'felix.althaus@undkonsorten.com, eike.starkmann@undkonsorten.com',
     'author_company' => 'undkonsorten, undkonsorten',
     'state' => 'beta',
-    'version' => '5.0.1',
+    'version' => '6.1.1',
     'constraints' => array(
         'depends' => array(
-            'typo3' => '10.4.0-12.99.99',
+            'typo3' => '11.5.0-12.99.99',
         ),
         'conflicts' => array(),
         'suggests' => array(
